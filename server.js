d95f5a83f0b9d78c0ee009054633b354700fe1be
Victor Nunez <vnunez0914@gmail.com>
2019-10-29 19:37:15 -0500

    mongoose db connected and added html to handlebars

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = ("express");
 var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 
 var PORT = process.env.PORT || 3000;
 
@@ -9,8 +11,32 @@ var router = express.Router();
 
 app.use(express.static(__dirname + "/public"));
 
+//connection express handlaebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// logs in request
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(router);
 
+//deployed db or local db
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connecting mongoose to db
+mongoose.connect(db, function(error){
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log("mongoose connected successfully");
+    }
+});
+
+// listening to port
 app.listen(PORT, function(){
-    console.log("listening on PORT :" + PORT);
+    console.log("listening on PORT: " + PORT);
 });
