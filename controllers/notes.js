808c6cc832fe937986d386114ab70a6852515895
Victor Nunez <vnunez0914@gmail.com>
2019-11-09 16:27:54 -0600

    controllers/schemas added

---
+++ controllers/notes.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema ({
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref: "Headline"
+    },
+    date: String,
+    noteText: String
+    
+});
+
+var Headline = mongoose.model("Note", noteSchema)
+
+module.exports= Note;
